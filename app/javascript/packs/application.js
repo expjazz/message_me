162f5f35e83b8b0f828929a93cfeae0feaa4bed4
Expedito Andrade <expeditojazz@gmail.com>
2020-05-12 15:59:37 -0300

    added favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,6 @@ require("semantic-ui-sass");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+$(document).on("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown();
+});
